c06e79e784638deab6a9509749bf503932bcb1ec
Guillermo Ojeda <gro1413@hotmail.com>
2023-10-18 18:32:54 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,50 @@
+public class App {
+    public static void main(String[] args){
+        
+        // TIPOS PRIMITIVOS
+
+        //Declaracion de una variable de tipo byte
+        byte edad = 21;
+        //System.out.println(edad);
+
+
+        //Declaracion de una variable de tipo short
+        short unShort = 204;
+        //System.out.println(unShort);
+
+
+        //Declaracion de una variable de tipo Int
+        int anio = 2024;
+        //System.out.println(anio);
+
+        
+        //Declaracion de una variable de tipo long
+        long unLong = 4000l;
+        //System.out.println(unLong);
+
+
+        //Declaracion de una variable de tipo de char
+        char letra = 'A';
+        //System.out.println(letra);
+        char numero = 87;
+        //System.out.println(numero);
+
+
+        //Declaracion de una variable de tipo bool
+        boolean trueOrFalse = true;
+        //System.out.println(trueOrFale);
+
+
+        //Declaracion de una variable de tipo float
+        float precio = 500.5014f;
+
+
+        //Declaracion de una variable de tipo double
+        double precio2 = 123.1234;
+
+
+        //Declaracion de una variabe de tipo String
+        String nombre = "Sirius";
+
+    }
+}
